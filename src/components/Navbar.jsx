8a6fd2bbaeff0ac7a6bffc0d20eac674d43e49b5
albertpalacio <you@example.com>
2020-07-17 10:04:41 -0400

    outline for navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="logo"> Smile</div>
+            <div className="donate-btn"> Donate</div>
+            <div className="hamb-menu"> Menu</div>
+        </div>
+    )
+}
+
+export default Navbar
