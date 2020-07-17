ab48c8a15991161e8d84f6cecb953f312c4329a2
albertpalacio <you@example.com>
2020-07-17 10:21:45 -0400

    homepage baseline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="logo"> Smile</div>
-            <div className="donate-btn"> <img src="https://i1.wp.com/www.hydesquare.org/wp-content/uploads/Blue-Donate-Button-Small.png?resize=191%2C98" alt="donate button" /></div>
+            <div className="donate-btn"> <a href ="https://mthsmile.com/donate/" >
+                <img src="https://i1.wp.com/www.hydesquare.org/wp-content/uploads/Blue-Donate-Button-Small.png?resize=191%2C98" alt="donate button" />
+                </a>
+                </div>
             <div className="hamb-menu"> Menu</div>
         </div>
     )
