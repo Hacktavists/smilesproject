4d99e78e442c6ea0c43649f5c2aec0343999920c
albertpalacio <you@example.com>
2020-07-17 16:36:59 -0400

    completed nav and homepage changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import '../styles/navbar.css'
+import React from "react";
+import "../styles/navbar.css";
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="logo"> 
-            <a href="/">
+            <a href="#">
                 <img 
                 src="https://i.imgur.com/7bs4i1k.png"
                 alt="smile trust logo"
                 />
             </a>
             </div>
-            <div className="donate-btn"> <a href ="https://mthsmile.com/donate/" >
+            <div className="donate-btn"> <a href ="https://mthsmile.com/donate/" alt="donate button" >
                 <button>DONATE</button>
                  </a>
                 </div>
-            {/* <div className="hamb-menu"> 
-            <button className="hamburger" id="hamburger">
-                <i class="fas fa-bars"></i>
-            </button>
-            </div> */}
+            <div className="hamb-menu"> 
+            <img src="https://i.imgur.com/GcFaQ2z.png"
+            alt="menu dropdown"
+            />
+           
+            </div>
+            
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
