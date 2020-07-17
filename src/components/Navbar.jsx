1458a4c5a2411c58574c94e4cd91068eb19c74d5
albertpalacio <you@example.com>
2020-07-17 10:14:00 -0400

    donate link added and img added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="logo"> Smile</div>
-            <div className="donate-btn"> Donate</div>
+            <div className="donate-btn"> <img src="https://i1.wp.com/www.hydesquare.org/wp-content/uploads/Blue-Donate-Button-Small.png?resize=191%2C98" alt="donate button" /></div>
             <div className="hamb-menu"> Menu</div>
         </div>
     )
